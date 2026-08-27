2cf7ab0d7df2911d78371cf245ca87b9f0c3d587
urielsantos <urielsantosh>
2026-08-26 23:43:05 -0700

    refactor: mejorar legibilidad del calculo de envio

--- src/com/tienda/CostoEnvio.java
+++ src/com/tienda/CostoEnvio.java
@@ -2,13 +2,20 @@ package com.tienda;
 
 public class CostoEnvio {
 
+    private static final double LIMITE_ENVIO_GRATIS = 1000;
+    private static final double LIMITE_ENVIO_REDUCIDO = 500;
+
+    private static final double COSTO_ENVIO_NORMAL = 100;
+    private static final double COSTO_ENVIO_REDUCIDO = 50;
+    private static final double COSTO_ENVIO_GRATIS = 0;
+
     public static double calcular(double importePedido) {
-        if (importePedido < 500) {
-            return 100;
-        } else if (importePedido < 1000) {
-            return 50;
+        if (importePedido < LIMITE_ENVIO_REDUCIDO) {
+            return COSTO_ENVIO_NORMAL;
+        } else if (importePedido < LIMITE_ENVIO_GRATIS) {
+            return COSTO_ENVIO_REDUCIDO;
         } else {
-            return 0;
+            return COSTO_ENVIO_GRATIS;
         }
     }
 }
